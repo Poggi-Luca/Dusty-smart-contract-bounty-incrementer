88fe557b8dc76843a41a07d803e0d713eba2cbcd
Poggi Luca <59626126+Poggi-Luca@users.noreply.github.com>
2020-08-24 15:32:44 +0200

    Add files via upload

---
+++ lib.rs
@@ -0,0 +1,56 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract(version = "0.1.0")]
+mod incrementer {
+    use ink_core::storage;
+
+    #[ink(storage)]
+    struct Incrementer {
+        value: storage::Value<i32>
+    }
+
+    impl Incrementer {
+        #[ink(constructor)]
+        fn new(&mut self, init_value: i32) {
+            self.value.set(init_value);
+        }
+        
+        #[ink(constructor)]
+        fn default(&mut self) {
+        	self.new(0)
+        }
+
+        #[ink(message)]
+        fn get(&self) -> i32 {
+            *self.value
+        }
+        
+        #[ink(message)]
+        fn inc(&mut self, by: i32) {
+        	*self.value += by;
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn default_works() {
+        	let contract = Incrementer::default();
+        	assert_eq!(contract.get(), 0);
+        }
+        
+        #[test]
+        fn it_works() {
+        	let mut contract = Incrementer::new(42);
+        	assert_eq!(contract.get(), 42);
+        	contract.inc(5);
+        	assert_eq!(contract.get(), 47);
+        	contract.inc(-50);
+        	assert_eq!(contract.get(), -3);
+        }
+    }
+}
